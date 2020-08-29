a2f643dd8d0235f2cf191107174f626b2576c54f
keerthana <keerrttinaa@gmail.com>
2020-08-29 17:49:35 -0400

    test

---
+++ scripts/before_install.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo apt install -y openjdk-8-jdk
---
+++ scripts/download_dumble.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
+    /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
